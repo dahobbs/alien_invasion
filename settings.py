5c3939599532344d9a21fd54abbf309b31c52f0c
david <david@pop-os.localdomain>
2019-08-23 18:41:37 +0100

    save progress

--- settings.py
+++ settings.py
@@ -1,1 +1,10 @@
+class Settings():
+    """A class to store all settings for alien invasion"""
+    def __init__(self):
+        """initialise the games settings"""
+        #screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
 
+        
