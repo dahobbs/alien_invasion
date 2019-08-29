a988ce5fcac777ffb0d46a7daaf34124d94ee0c8
david <david@pop-os.localdomain>
2019-08-29 14:29:31 +0100

    refactored bullet code into thier own functions

--- game_functions.py
+++ game_functions.py
@@ -19,13 +19,26 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key ==pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings,screen,ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings,screen,ship,bullets)
 def check_keyup_events(event,ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    if len(bullets)< ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings,screen,ship)
+        bullets.add(new_bullet)
+
+
+def update_bullets(bullets):
+    bullets.update()
+
+    #get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <=0:
+            bullets.remove(bullet)
 
 def update_screen(ai_settings,screen,ship,bullets):
     #redraw the screen during each pass of the loop
@@ -34,7 +47,7 @@ def update_screen(ai_settings,screen,ship,bullets):
     #redraw all bullets behind ship and aliens
     for bullet in bullets.sprites():
         bullet.draw_bullet()
-        
+
     ship.blitme()
     #make the most recently drawn screen visible
     pygame.display.flip()
